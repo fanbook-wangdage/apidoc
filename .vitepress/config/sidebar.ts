cd8f237f40cd66d33bc8039ebfad3ec153eb303e
fanbook-wangdage <fbwdg@outlook.com>
2024-04-10 13:15:30 +0800

    改用vitepress

---
+++ .vitepress/config/sidebar.ts
@@ -0,0 +1,18 @@
+/**
+ * 侧边栏菜单
+ *
+ * @see sidebar https://vitepress.vuejs.org/guide/theme-sidebar#sidebar
+ */
+export const sidebar = {
+  '/docs/': [
+    // {
+    //   text: '快速上手',
+    //   collapsible: true,
+    //   link: '/docs/hello',
+    //   items: [
+    //     { text: 'hello', link: '/docs/hello' },
+    //     { text: '欢迎使用', link: '/docs/welcome' }
+    //   ]
+    // }
+  ]
+}
--- .vuepress/navbar.ts
+++
@@ -1,8 +0,0 @@
-import {NavItem} from "vuepress/config";
-
-export default [
-    {
-        text: "API文档",
-        link: '/API文档/'
-    },
-] as NavItem[];
